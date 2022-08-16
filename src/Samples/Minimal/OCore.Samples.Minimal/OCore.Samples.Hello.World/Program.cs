f833b22c0f39115d71db0935023f394ecf04bdf2
COCPORN <cocporn@gmail.com>
2022-08-16 15:06:58 +0200

    Remove namespace

--- src/Samples/Minimal/OCore.Samples.Minimal/OCore.Samples.Hello.World/Program.cs
+++ src/Samples/Minimal/OCore.Samples.Minimal/OCore.Samples.Hello.World/Program.cs
@@ -1,47 +1,36 @@
-﻿using OCore.Entities.Data;
-using OCore.Entities.Data.Extensions;
-using OCore.Services;
-using Orleans;
-
+﻿using OCore.Services;
 await OCore.Setup.DeveloperExtensions.LetsGo();
 
-namespace HelloWorld
+[Service("HelloWorld")]
+public interface IHelloWorldService : IService
 {
-    [Service("HelloWorld")]
-    public interface IHelloWorldService : IService
-    {
-        Task<string> SayHelloTo(string name);
+    Task<string> SayHelloTo(string name);
 
-        Task<string> ShoutHelloTo(string name);
+    Task<string> ShoutHelloTo(string name);
+}
+
+public class HelloWorldService : Service, IHelloWorldService
+{
+    readonly INameCapitalizationService nameCapitalizationService;
+
+    public HelloWorldService(INameCapitalizationService nameCapitalizationService)
+    {
+        this.nameCapitalizationService = nameCapitalizationService;
     }
 
-    public class HelloWorldService : Service, IHelloWorldService
-    {
-        readonly INameCapitalizationService nameCapitalizationService;
+    public Task<string> SayHelloTo(string name) => Task.FromResult($"Hello, {name}!");
 
-        public HelloWorldService(INameCapitalizationService nameCapitalizationService)
-        {
-            this.nameCapitalizationService = nameCapitalizationService;
-        }
+    public async Task<string> ShoutHelloTo(string name)
+        => await SayHelloTo(await nameCapitalizationService.Capitalize(name));
+}
 
-        public Task<string> SayHelloTo(string name)
-            => Task.FromResult($"Hello, {name}!");
+[Service("Capitalization")]
+public interface INameCapitalizationService : IService
+{
+    Task<string> Capitalize(string name);
+}
 
-        public async Task<string> ShoutHelloTo(string name)
-            => await SayHelloTo(await nameCapitalizationService.Capitalize(name));
-    }
-
-    [Service("Capitalization")]
-    public interface INameCapitalizationService : IService
-    {
-        Task<string> Capitalize(string name);
-    }
-
-    public class NameCapitalizationService : Service, INameCapitalizationService
-    {
-        public Task<string> Capitalize(string name)
-        {
-            return Task.FromResult(name.ToUpper());
-        }
-    }
+public class NameCapitalizationService : Service, INameCapitalizationService
+{
+    public Task<string> Capitalize(string name) => Task.FromResult(name.ToUpper());    
 }
