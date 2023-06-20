8e119f340507531f4c3d2a72122d1ce3d05e934a
Lars Thomas Denstad <lars-thomas.denstad@payex.com>
2023-06-20 15:48:03 +0200

    Add Guid-keys for getting DataEntities

--- src/OCore/OCore.Authorization/ISystemService.cs
+++ src/OCore/OCore.Authorization/ISystemService.cs
@@ -10,7 +10,8 @@ namespace OCore.Authorization
         /// <summary>
         /// Setup the system for first use using a Guid token
         /// </summary>
-        /// <param name="token"></param>
+        /// <param name="token">The seed token for the system.</param>
+        /// <param name="accountId">The account ID to promote to root.</param>
         /// <returns></returns>
         [Authorize(requirements: Requirements.None)]
         Task Initialize(string token, string accountId);
--- src/OCore/OCore.Entities.Data/Extensions/GetDataEntityExtensions.cs
+++ src/OCore/OCore.Entities.Data/Extensions/GetDataEntityExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Orleans;
 
 namespace OCore.Entities.Data.Extensions
@@ -13,6 +14,16 @@ namespace OCore.Entities.Data.Extensions
         public static T GetDataEntity<T>(this IGrainFactory grainFactory, string prefix, string identity) where T : IDataEntity
         {
             return grainFactory.GetGrain<T>($"{prefix}:{identity}");
+        }
+
+        public static T GetDataEntity<T>(this IGrainFactory grainFactory, Guid key) where T : IDataEntity
+        {
+            return grainFactory.GetGrain<T>(key.ToString());
+        }
+        
+        public static T GetDataEntity<T>(this IGrainFactory grainFactory, string prefix, Guid identity) where T : IDataEntity
+        {
+            return grainFactory.GetGrain<T>($"{prefix}:{identity.ToString()}");
         }
     }
 }
