d3fc51f4e7a23ef76451b6768c7a0ec2d93f2e83
Lars Thomas Denstad <lars-thomas.denstad@payex.com>
2023-06-22 12:46:23 +0200

    Add test for correlation ID, see that it's echoed

---
+++ src/OCore/OCore.Tests/Http/CorrelationId/CorrelationIdTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using OCore.Testing.Fixtures;
+using OCore.Testing.Host;
+using OCore.Tests.Seeders.Zoo;
+
+namespace OCore.Tests.Http.CorrelationId;
+
+public class CorrelationIdTests : FullHost<ZooSeeder>
+{
+    [Fact]
+    public async void CheckCorrelationIdMatch()
+    {
+        using var requestMessage =
+            new HttpRequestMessage(HttpMethod.Get, $"/data/Animal/Dog");
+
+        requestMessage.Headers.Add("correlationId", "something");
+
+        var response = await HttpClient.SendAsync(requestMessage);
+        
+        var body = await response.Content.ReadAsStringAsync();
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        // Check that the correlationId is in the response header
+        Assert.Equal("something", response.Headers.GetValues("correlationId").First());
+        Assert.Contains("Rover", body);
+    }
+
+    public CorrelationIdTests(FullHostFixture<ZooSeeder> fixture) : base(fixture)
+    {
+    }
+}
