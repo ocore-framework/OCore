d34eb75657f7ae832de42a8159648803f4de62bb
Lars Thomas Denstad <lars-thomas.denstad@payex.com>
2023-06-22 14:35:34 +0200

    ...And add these, perhapsly?

---
+++ src/OCore/OCore.Http.Hateoas/HateoasLink.cs
@@ -0,0 +1,11 @@
+﻿namespace OCore.Http.Hateoas;
+
+[GenerateSerializer]
+public class HateoasLink
+{
+    [Id(0)] public string Href { get; set; } = string.Empty;
+
+    [Id(1)] public string Rel { get; set; } = string.Empty;
+
+    [Id(2)] public string Method { get; set; } = string.Empty;
+}
