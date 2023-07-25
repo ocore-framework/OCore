f7dfb98ca7b7074893b46965562e5f52ce4cf63c
Lars Thomas Denstad <cocporn@gmail.com>
2023-07-25 17:39:52 +0200

    Some wild, untested code, as per usual

--- src/OCore/OCore.Data.Entities.SignalR/DataEntityHub.cs
+++ src/OCore/OCore.Data.Entities.SignalR/DataEntityHub.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Concurrent;
+using System.Threading.Tasks.Dataflow;
 using Microsoft.AspNetCore.SignalR;
 using OCore.Entities.Data;
 
@@ -7,19 +8,36 @@ namespace OCore.Data.Entities.SignalR;
 public class DataEntityHub : Hub
 {
     private readonly ConcurrentDictionary<Type, DataEntityStreamer> _streamers = new();
+
+    private readonly ConcurrentDictionary<string, List<string>> _groups = new();
+
     //private readonly ConcurrentDictionary<string, string> _connectionsToDataEntities = new();
     private readonly IClusterClient _clusterClient;
-    
+
     public DataEntityHub(IClusterClient clusterClient)
     {
         _clusterClient = clusterClient;
     }
-    
-    public async Task Subscribe(string dataEntityType, string dataEntityId)
+
+    public async Task Subscribe(string dataEntityTypeString, string dataEntityId)
     {
-        await Groups.AddToGroupAsync(Context.ConnectionId, $"{dataEntityType}:{dataEntityId}");
-        var streamer = CreateIfNotExists(dataEntityType, dataEntityId);
-        await streamer.Setup(dataEntityType, )
+        var groupName = $"{dataEntityTypeString}:{dataEntityId}";
+        await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+        _groups.AddOrUpdate(Context.ConnectionId,
+            (_) => { return new List<string> { groupName }; },
+            (_, l) =>
+            {
+                l.Add(groupName);
+                return l;
+            });
+        var streamer = CreateIfNotExists(dataEntityTypeString, dataEntityId);
+        var dataEntityTypeParam = Type.GetType(dataEntityTypeString);
+        if (dataEntityTypeParam is null)
+        {
+            return;
+        }
+        var dataEntityType = typeof(IDataEntity<>).MakeGenericType(dataEntityTypeParam);
+        var _ = streamer.Stream(dataEntityType, dataEntityId);
     }
 
     private DataEntityStreamer CreateIfNotExists(string dataEntityType, string dataEntityId)
@@ -29,11 +47,15 @@ public class DataEntityHub : Hub
         {
             throw new ArgumentException($"Type not found");
         }
-        
+
         if (_streamers.TryGetValue(type, out var streamer) == false)
         {
             streamer = new DataEntityStreamer(this, _clusterClient);
             _streamers.TryAdd(type, streamer);
+            return streamer;
+        }
+        else
+        {
             return streamer;
         }
     }
@@ -41,8 +63,9 @@ public class DataEntityHub : Hub
     public async Task Unsubscribe(string dataEntityType, string dataEntityId)
     {
         await Groups.RemoveFromGroupAsync(Context.ConnectionId, $"{dataEntityType}:{dataEntityId}");
+        // TODO: Should be removed from _groups and the streamer should be stopped if no more connections
     }
-    
+
     public async Task Update(string dataEntityType, string dataEntityId, string dataEntityUpdate)
     {
         await Clients.Group($"{dataEntityType}:{dataEntityId}").SendAsync("Update", dataEntityUpdate);
@@ -52,11 +75,18 @@ public class DataEntityHub : Hub
     {
         await Clients.Group($"{dataEntityType}:{dataEntityId}").SendAsync("Deleted");
     }
-    
-    public override Task OnDisconnectedAsync(Exception? exception)
+
+    public override async Task OnDisconnectedAsync(Exception? exception)
     {
-        Context.ConnectionId
-        return base.OnDisconnectedAsync(exception);
+        if (_groups.TryGetValue(Context.ConnectionId, out var groups))
+        {
+            foreach (var group in groups)
+            {
+                await Groups.RemoveFromGroupAsync(Context.ConnectionId, group);
+            }
+        }
+
+        await base.OnDisconnectedAsync(exception);
     }
 }
 
@@ -66,7 +96,7 @@ internal class DataEntityStreamer
     private readonly bool _jsonDiff;
     private readonly HashSet<string> _connections = new();
     private readonly IClusterClient _clusterClient;
-    
+
     public DataEntityStreamer(DataEntityHub hub,
         IClusterClient clusterClient,
         bool jsonDiff = true)
@@ -75,18 +105,22 @@ internal class DataEntityStreamer
         _clusterClient = clusterClient;
         _jsonDiff = jsonDiff;
     }
-    
-    public async Task Setup(Type type, string id)
+
+    public async Task Stream(Type type, string id)
     {
         var grain = (IDataEntity)_clusterClient.GetGrain(type, id);
+        var typeString = type.ToString();
         await foreach (var update in grain.GetJsonUpdates(_jsonDiff))
         {
-            
+            await _hub.Update(typeString, id, update);
         }
+
+        await _hub.Deleted(typeString, id);
     }
 
     public Task Stop()
     {
-        
+        // TODO: Implement
+        return Task.CompletedTask;
     }
 }
---
+++ src/OCore/OCore.Data.Entities.SignalR/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace OCore.Data.Entities.SignalR;
+
+public static class Extensions
+{
+    public static IServiceCollection AddSignalRDataEntityHub(this IServiceCollection services)
+    {
+        return services
+            .AddSingleton<DataEntityHub>();
+    }
+}
